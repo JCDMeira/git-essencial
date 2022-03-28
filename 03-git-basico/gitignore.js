c7e20f9282ebecf947d6faf1308d4753c8929ac0
Jean Meira <jean.meira10@hotmail.com>
2022-03-28 16:45:10 -0300

    doc: add how to install default gitignore

--- 03-git-basico/gitignore.js
+++ 03-git-basico/gitignore.js
@@ -4,3 +4,7 @@
 // - ignorando todos os arquivos da mesma extensão ex: *.sqlite3
 // -Ignorar diretórios -> ex: /03-git-basico
 // -Ignorar diretórios (buscando sub-diretórios também) -> ex: **03-git-basico
+
+// ! para instalar uma versão padrão de gitigore use:
+// ? npx gitignore node
+// # virá com diversos cases padrões, e é possível adicionar ou remover o que desejar.
