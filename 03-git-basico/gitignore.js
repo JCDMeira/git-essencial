d59fbc180dfef4d8b4f9f8c49ad4a2ff4023bf40
Jean Meira <jean.meira10@hotmail.com>
2022-03-28 16:43:24 -0300

    doc: add explanation about gitignore

---
+++ 03-git-basico/gitignore.js
@@ -0,0 +1,6 @@
+// - arquivo .gitignore serve para ignorar arquivos e suas mudanças. O git não irá "olhar" para eles
+
+// - ignorando arquivo ex: db.sqlite3
+// - ignorando todos os arquivos da mesma extensão ex: *.sqlite3
+// -Ignorar diretórios -> ex: /03-git-basico
+// -Ignorar diretórios (buscando sub-diretórios também) -> ex: **03-git-basico
