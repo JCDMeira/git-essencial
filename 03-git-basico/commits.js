8f61e03dc8828167bfcf60fbd3dbe8bc6d2a7cd0
Jean Meira <jean.meira10@hotmail.com>
2022-04-02 15:44:45 -0300

    feat: add notes about commits

---
+++ 03-git-basico/commits.js
@@ -0,0 +1,7 @@
+/*
+    - commits são a passagem dos arquivos que estão trackeados para o estatus staged.
+    - deixando eles prontos para enviar ou submeter para o repositório remoto (principal).
+    ? git commit -m "sua mensagem aqui"
+
+    - Cada commit gera um hash id único, que serve de identificador do commit
+*/
