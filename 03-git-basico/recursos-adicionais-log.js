b1180ba4c02b5e37226870f6c310bc133d8923c5
Jean Meira <jean.meira10@hotmail.com>
2022-04-02 16:28:05 -0300

    feat: add notes more about log and log flags

---
+++ 03-git-basico/recursos-adicionais-log.js
@@ -0,0 +1,24 @@
+/*
+    _ É possível colocar um número n, mostrando apenas os últimos n commits
+    ? git log -2
+    - mostra apenas os últimos 2 commits
+
+    _ mostrar os últimos n commits de modo resumido
+    ? git log --oneline -2
+    - últimos 2 commits em uma linha cada
+
+    _pesquisar commits anteriores a uma data:
+    ?git log --before="2022-04-01"
+
+    _pesquisar commits posteriores a uma data:
+    ?git log --after="2022-04-01"
+
+    _pesquisar commits posteriores a uma data:
+    ?git log --after="2022-04-01"
+
+    _pesquisar commits desde:
+    ?git log --since="2 days ago"
+
+    _pesquisar commits por autor:
+    ?git log --author="jean"
+*/
