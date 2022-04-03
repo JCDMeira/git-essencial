bc73dfc3b74d6f23b6abcb2118bdcca76a939850
Jean Meira <jean.meira10@hotmail.com>
2022-04-03 11:40:05 -0300

    feat: add notes about navigation on commits

---
+++ 03-git-basico/navegando_em_commits.js
@@ -0,0 +1,10 @@
+/*
+    _ É possível navegar entre os commits do hisstórico, assim voltando como o projeto estava naquele momento
+    _ para isso basta usar o comando de checkout com o hash id do commit.
+    ? git checkout hash_id
+
+    _ para para voltar para  master basta usar o checkout com master:
+    ? git checkout master
+
+
+*/
