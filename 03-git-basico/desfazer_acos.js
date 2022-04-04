1b3873cddac9795009c778eccaa4ffaa5503a154
Jean Meira <jean.meira10@hotmail.com>
2022-04-04 19:26:20 -0300

    feat: add notes about ammend command

---
+++ 03-git-basico/desfazer_acos.js
@@ -0,0 +1,10 @@
+/*
+    _ corrigir uma mensagem errada no commit
+    ? git commit --amend -m "mensagem corrigida"
+    # amend significa alterar/altera
+    - é um novo commit, mas ele adiciona as novas coisas adicionadas também e altera a mensagem
+
+    ? git commit --amend --no-edit
+    - adiciona as novas alterações e faz um novo commit, mas não altera a mensagem
+
+*/
