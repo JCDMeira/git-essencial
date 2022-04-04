e7254530a4b96f54d552724aed43087603a5544f
Jean Meira <jean.meira10@hotmail.com>
2022-04-03 22:04:18 -0300

    feat: add notes about other commands

---
+++ 03-git-basico/outros_comandos.js
@@ -0,0 +1,23 @@
+/*
+    _ renomear arquivos através do git:
+    ? git mv nome_atual nome_novo
+    # mv de move
+
+    _ remover arquivos com o git:
+    ? git rm nome_arquivo
+    #rm de remove
+
+    _ verificar diferenças de um arquivo staged com diff
+    ? git diff --staged
+    # compara o que está pronto para ser commitado com o que está no último commit
+
+    _ é possível fazer comparação com um commit anterior.
+    ? git diff hash_id
+
+    _ é possível fazer comparação entre dois commits anteriores. 
+    ? git diff hash_id1..hash_id2
+    - (1 - mais velho na linha do tempo)
+    - (2 - mais novo na linha do tempo)
+
+    # diff vem de difference
+*/
