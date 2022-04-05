4d6a4cdc71c32021536ccfc221295055f12b3a21
Jean Meira <jean.meira10@hotmail.com>
2022-04-04 22:23:15 -0300

    feat: add notes about undo changes

---
+++ 03-git-basico/removendo_de_staged.js
@@ -0,0 +1,16 @@
+/*
+    _ retirando um arquivo de staged com:
+    ? git restore --staged nome_arquivo
+    
+    _ usar checkout para voltar 
+    ? git checkout nome_arquivo
+    #volta os que está modificado para o estado do último commit
+
+    _ voltar o estado de vários arquivos com o
+    ? git reset HEAD --hard
+    #volta todos os que estão modificados para o estado do último commit
+
+    _ Resetar o último commit e descartar as alterações 
+    ? git reset HEAD^ --hard
+    # volta o estado em um commit, e retira as alterações
+*/
