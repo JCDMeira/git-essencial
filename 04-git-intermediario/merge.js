9175d1713e794e5f5b9544c71e68c8e5bd02fb5a
Jean Meira <jean.meira10@hotmail.com>
2022-04-06 20:29:24 -0300

    feat: add notes about merge

---
+++ 04-git-intermediario/merge.js
@@ -0,0 +1,7 @@
+/*
+    _ O comando merge uni dois ramos (branchs)
+    # esteja no ramo destino (exemplo: estando na master)
+    ? git merge feat001
+
+    - joga as alterações de feat001 na master
+*/
