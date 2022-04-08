e35e5409bcda0a7968ba5873d6928a6ad7b609ff
Jean Meira <jean.meira10@hotmail.com>
2022-04-08 13:49:36 -0300

    feat: add notes about bare repository

---
+++ 04-git-intermediario/bare_ropository.js
@@ -0,0 +1,8 @@
+/*
+    _ iniciando um bare repository
+    ? git init --bare
+
+    _ bare repository é algo como um "repositório desprotegido/destravado", pois aceita commits de 
+    _ outros repositórios non-bare, um remoto que aceita push de repositórios locais além do dono do 
+    _ projeto ou de pessoas inseridas nele.
+*/
