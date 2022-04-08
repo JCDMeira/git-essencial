d1990aa5925a8013c86d069aa766d068709129e0
Jean Meira <jean.meira10@hotmail.com>
2022-04-08 14:05:40 -0300

    feat: add section 05 - git avançado

---
+++ 05-git-avancado/topicos.js
@@ -0,0 +1,12 @@
+/*
+    - Navegando e conhecendo o gitHub
+    - Criando repositórios pele gitHub
+    - Fazendo uso do Clone e Pull
+    - Adicionar colaboradores ao projeto
+    - Consultar histórico de commits
+    - Desfazendo checkouts
+    - Desfazendo Revert e Reset
+    - Resolvendo conflitos
+    - Conflitos de Merge
+    - Histórico com uma interface gráfica
+*/
