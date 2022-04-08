277ca3adc4b1d5479b45e1ff1e4699da0ea1489a
Jean Meira <jean.meira10@hotmail.com>
2022-04-08 14:01:44 -0300

    feat: add notes about tags

---
+++ 04-git-intermediario/tags.js
@@ -0,0 +1,16 @@
+/*
+    _ tags geram uma versão pronta pra uso, mesmo que ela seja incompleta
+    ! pode também gerar uma release, uma versão completa, V1 por exemplo
+
+    ? git tag minha_tag
+
+    _ deve ser subido com a tag
+    ? git push origin minha_tag
+
+    ! estando na tag o repositório não aceita mais commits
+    _ criar um branch a partir da tag
+    ? git switch -c nova_branch
+
+    @ Se corrigiria o que fosse necessário na branch de correção, e então faria os commits, megearia na master e criaria a nova versão estável. (uma nova tag)
+    @ esse poderia ser V1.1 por exemplo, isso dependendo dos pachs de nomeação que estão sendo usadas.
+*/
