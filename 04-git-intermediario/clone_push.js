3951be5d2f43e98055462d49f1fe221e6aee64fe
Jean Meira <jean.meira10@hotmail.com>
2022-04-07 22:01:21 -0300

    feat: add notes about clone and push

---
+++ 04-git-intermediario/clone_push.js
@@ -0,0 +1,8 @@
+/*
+    _ clone literalmente clona um repositório existente
+    ? git clone endereco_repositorio
+
+    _ push envia os commits para o repositório remoto
+    ? git push 
+    # o primeiro push terá que indicar a origim
+*/
