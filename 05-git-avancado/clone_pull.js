7b5ee3ba0d2444ded20382be2e1bf28eec44579b
Jean Meira <jean.meira10@hotmail.com>
2022-04-09 16:39:13 -0300

    feat: add notes about clone and pull

---
+++ 05-git-avancado/clone_pull.js
@@ -0,0 +1,9 @@
+/*
+    _ permite criar uma cópia local de um repositório remoto
+    ? git clone endereco_rep_remoto
+
+    _ já vincula com o repositório
+
+    _ Pega atualização da origin indicada 
+    ? git pull origin master
+*/
