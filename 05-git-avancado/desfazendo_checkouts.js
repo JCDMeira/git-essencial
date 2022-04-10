60d94acba0552a7c4c35ffca7d883fcd845c743b
Jean Meira <jean.meira10@hotmail.com>
2022-04-10 15:15:16 -0300

    feat: add notes about undoing checkout

---
+++ 05-git-avancado/desfazendo_checkouts.js
@@ -0,0 +1,10 @@
+/*
+    _ voltar todos arquivos ao estado original
+    ? git checkout -- .
+    
+    _ voltar um arquivo ao estado original
+    ? git checkout -- fine_name
+
+    _ tirando arquivos já adicionados, e voltando para estado anterior
+    ? git checkout HEAD -- .
+*/
