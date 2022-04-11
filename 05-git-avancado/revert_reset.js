df5374c8c0b50b229a1bde84013d119c63c08ca3
Jean Meira <jean.meira10@hotmail.com>
2022-04-11 14:36:09 -0300

    feat: add notes about revert and reset

---
+++ 05-git-avancado/revert_reset.js
@@ -0,0 +1,10 @@
+/*
+    _ revert irá criar um commit que faz ações contrárias ao que foi feito
+    ? git revert hasj_id 
+    @ isso mantém fielmente o histórico de tudo que aconteceu
+    ! exemplo: ao ter um problema e testar soluções você mantém o histórico dos testes, sabendo o que já tentou
+
+    _ git reset deleta mesmo o commit
+    ? git reset HEAD~1 
+    # HEAD é simbolico ao topo e ~numero_desejado é quanto commits a partir do topo se deseja deletar
+*/
