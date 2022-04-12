8b05130f1809fb950e685e483b11730e54a03e06
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 20:29:31 -0300

    feat: add notes about fork

---
+++ 06-colaborando-com-open-source/fork.js
@@ -0,0 +1,8 @@
+/*
+    _ Fork de um repositório cria uma estrutura igual ao repositório original
+    _ mas ele é do seu usuário, e é citado que o repositório foi feito a partir de outro repositório.
+
+    @ Reatribuir uma origin caso presice
+    ? git remote -v
+    ? git remote set-url origin caminho_rep_remoto
+*/
