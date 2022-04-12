46636f228c72775e2662abe1de08fb9a3a2c161d
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 18:00:50 -0300

    feat: add notes about issues

---
+++ 06-colaborando-com-open-source/isssues.js
@@ -0,0 +1,8 @@
+/*
+    _ issues são problemas a serem resolvidos no seu projeto
+
+    _ cria um erro, indicando em que lugar deveria ser corrigido e pode indicar a correção
+
+    _Fechar issue de modo automático com o commit
+    @ adicionar ao fim da mensagem de commit Closes #1
+*/
