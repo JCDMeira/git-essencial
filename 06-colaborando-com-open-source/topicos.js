d531e9c940cb3e2c5e5b34eab2d61070f73b1b28
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 17:38:51 -0300

    feat: add warning message

--- 06-colaborando-com-open-source/topicos.js
+++ 06-colaborando-com-open-source/topicos.js
@@ -6,4 +6,5 @@
     - Verificando e aceitando pull request
     - Work flow padrão do gitHub
 
+    ! Claro que o desenvolvedor tem que saber usar o terminal, porque não é sempre que você vai ter a interface gráfica para fazer uso dos recursos. E mais, se você utilizar a interface gráfica você vai clicar em botões você não vai conhecer os comandos, e se você não conhecer os comandos você não é nada. Primeiro deve masterizar os comandos do git no terminal, e depois entender como usar a interface gráfica.
 */
