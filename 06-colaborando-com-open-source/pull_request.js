6d067008c1073389c66fc484701b00d36898b4b3
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 20:45:46 -0300

    feat: add notes about receiving a pull request

--- 06-colaborando-com-open-source/pull_request.js
+++ 06-colaborando-com-open-source/pull_request.js
@@ -1,4 +1,10 @@
 /*
     _ pull-request são a maneira que o repositório que criamos a partir do fork podem ser solicitados 
     _ a entrar no repositório original
+
+    _ ao receber um fork se deve pegar o id, e recomendasse que baixe o repositório no local e teste
+    _verificando a integridade do código e as funcionalidades.
+    _baixa as alterações de um fork recebido passando o id da requisição de fork
+    ? git fetch origin pull/3/head:correcao_i2
+    # após o head é o nome da nova branch
 */
