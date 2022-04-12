9a66a1191f9c258356045c31525bfa979e13dac8
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 17:48:23 -0300

    feat: add find repositories

---
+++ 06-colaborando-com-open-source/descobrindo_avaliando.js
@@ -0,0 +1,5 @@
+/*
+    _ É possível achar os mais diversos projetos no gitHub
+    _ ele é uma rede social para programadores, que nos permite ver outros desenvolvedores ou projetos
+    _ o código fonte, e até clonar e ver mais sobre o projeto
+*/
