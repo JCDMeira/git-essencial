2c4940a281bc7bcef205ed92f26ca1719c880613
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 15:24:06 -0300

    feat: add notes about resolving conflicts

---
+++ 05-git-avancado/resolvendo_conflitos.js
@@ -0,0 +1,9 @@
+/*
+    _ acontecem conflitos quando o remoto está mais atualizado que o local
+    _ atualizar os arquivos locais, com ou sem a indicação de origin
+    ? git pull -u oirin master
+    ? git pul
+    #vai gerar conflito, sendo só apagar o que precisa dentro das marcações
+
+    @ se abrir terminal e não tiver alterações ou conflitos, se usa o gravar -> enter -> sair
+*/
