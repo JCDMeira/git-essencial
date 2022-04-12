020aa589fb3328b6f88163a689618f54fa272606
Jean Meira <jean.meira10@hotmail.com>
2022-04-12 15:43:23 -0300

    feat: add section 06 - open source

---
+++ 06-colaborando-com-open-source/topicos.js
@@ -0,0 +1,9 @@
+/*
+    - Descobrir e avaliar projetos
+    - Trabalhando con issues
+    - Trabalhando com fork
+    - Realizando pull request
+    - Verificando e aceitando pull request
+    - Work flow padrão do gitHub
+
+*/
