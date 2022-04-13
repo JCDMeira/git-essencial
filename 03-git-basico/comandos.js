689817a357ff37a229238cfb1c710b1b8b39fa0b
Jean Meira <jean.meira10@hotmail.com>
2022-04-13 09:45:26 -0300

    fix: email

--- 03-git-basico/comandos.js
+++ 03-git-basico/comandos.js
@@ -1,11 +1,11 @@
 /* 
     - Adiciona credenciais de nome e e-mail de forma global (para o computador).
     ? git config --global user.name "Fulano de Tal"
-    ? git config --global user.email fulanodetal@exemplo.br
+    ? git config --global user.email "fulanodetal@exemplo.br"
 
     - Pode não usar o flag global se for para configurações apenas local (no projeto em questão)
     ? git config  user.name "Fulano de Tal"
-    ? git config  user.email fulanodetal@exemplo.br
+    ? git config  user.email "fulanodetal@exemplo.br"
 
     - Mostra os estados atuais dos arquivos do projeto
     ? git status
