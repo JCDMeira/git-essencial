0ea9adb4812f14d4ad24c3df5c4fd46029305b30
Jean Meira <jean.meira10@hotmail.com>
2022-04-15 15:07:52 -0300

    feat: add section 'adicionais'

---
+++ adicionais/ssh.js
@@ -0,0 +1,9 @@
+/*
+    ! https://docs.github.com/pt/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+    _ 1 - abra o terminal
+    _ 2 - ssh-keygen -t ed25519 -C "your_email@example.com"
+    Quando aparecer a solicitação "Enter a file in which to save the key" 
+    (Insira um arquivo no qual salvar a chave), 
+    presssione Enter. O local padrão do arquivo será aceito.
+*/
