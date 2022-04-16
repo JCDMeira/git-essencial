ad392545da8727fd978474d098eabe817d4baef2
Jean Meira <jean.meira10@hotmail.com>
2022-04-16 14:28:50 -0300

    feat: add notes about cherry-pick and blame

---
+++ adicionais/cherrypock_and_blame.js
@@ -0,0 +1,24 @@
+/*
+    | git Cherry-pick
+
+    _ aplica as alterações de um commit na branch atual
+    _ cria um novo commit
+    _ útil para recuperar histórico
+
+    ? git cherry-pick hash_id
+    ? git cherry-pick nome_da_branch
+    ? git cherry-pick nome_da_branch~2 (pega o commit de dois commits atrás)
+
+    @ cherry-pick sempre pega apenas um commit
+    @ o commando adiciona todas as mudanças de um commit indicado 
+    @ e aplica um novo commit com as mesmas alterações e mesma 
+    @ mensagem, mas em um novo commit
+
+    | git Blame
+
+    _ mostra as alterações feitas em um arquivo por linha
+    _ mostra o autor e o commit que foi feito aquela linha
+    _ útil para verifica quando as alterações foram feitas e por quem
+
+    ? git blame nome_do_arquivo
+*/
