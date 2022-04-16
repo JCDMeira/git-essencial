36ee854d69cd880a60c907cdee5c2d2f411834af
Jean Meira <jean.meira10@hotmail.com>
2022-04-16 14:22:15 -0300

    feat: add notes about stash

---
+++ adicionais/stash.js
@@ -0,0 +1,18 @@
+/*
+    _ guarta alterações do working directory (diretório que está trabalhando)
+    _ com isso permite fazer rebase, merge ou trocar de branch sem commitar as mudanças
+
+    ? git stash
+    # guarda as mudanças atuais em um "cache"
+
+    ? git stash list
+    # lista os stash's guardados
+
+    ? git stash poop
+    # reaplica as alterações do último stash
+
+    @ muito últil também para pegar alterações do local, criar uma branch ou ir para uma já criada 
+    @ e então soltar as alterações ali
+
+    _ stash funciona como uma pilha
+*/
