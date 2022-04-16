dbd253d0c5e1907fb112fcc1b04475f8e7d2e7e1
Jean Meira <jean.meira10@hotmail.com>
2022-04-16 14:52:31 -0300

    feat: add notes about milestones

---
+++ adicionais/milestones.js
@@ -0,0 +1,10 @@
+/*
+    _ milestones são grupos de issues associados a um objetivo
+    _ possui uma data
+    _ permite acompanhar a % de issues resolvidas
+    _ existe em outros servidores git
+
+    @ agrupa issues e faz acompanhar cada fechada
+    _ referência
+    ! https://docs.github.com/pt/issues/tracking-your-work-with-issues/about-issues
+*/
